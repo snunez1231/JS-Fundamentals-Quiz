dad22bb6f0532869b9927783b11fd5b88efc400c
stephanie nunez <snunez1231@gmail.com>
2023-10-31 16:21:47 -0400

    completed checkanswer fuction inorder to make sure user inputed the correct answer to question

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,49 +2,75 @@ var startButton = document.querySelector('#start-btn')
 var questionsContainer = document.querySelector('#question-container')
 var resultContainer = document.querySelector('#result-container')
 var questionElement = document.querySelector('#questions')
-var answers= document.querySelector('#answers-container')
+var answers = document.querySelector('#answers-container')
 var submitButton = document.querySelector('#submit-btn')
 var quizcontainer = document.getElementById('quiz-container')
 
-var display = 0;
 var QuestionIndex = 0;
 var score = 0;
-var timeLeft = 60;
+// var timeLeft = 60;
 
 var questions = [
-    // Add questions here
+    
     {
         question: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
     },
     {
-        question: "Example 2",
-        choices: ["Choice1", "Choice2"],
-        answer: "Choice2"
+        question: "What does DOM stand for in JavaScript?",
+        choices: ["Document Object Model", "Data Object Mode", "Dynamic Object Method", "Document Oriented Module"],
+        answer: "Document Object Model"
+    },
+    {
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        choices: ["var", "int", "variable", "v"],
+        answer: "var"
+    },
+    {
+        question: "What is the result of the expression 5 == '5' in JavaScript?",
+        choices: ["true", "false", "undefined", "error"],
+        answer: "true"
     }
+
 ];
 
 
 
 
 function setNextQuestion() {
-    var currentQuestion= questions[QuestionIndex]
-    questionElement.textContent=currentQuestion.question
-    for ( i=0 ; i< currentQuestion.choices.length; i++){ 
-    var currentanswer=currentQuestion.choices[i]
-    var questionbtn= document.createElement("button")
-    questionbtn.textContent=currentanswer
-    answers.append(questionbtn)
-    questionbtn.addEventListener("click", checkAnswer)
+    var currentQuestion = questions[QuestionIndex]
+    questionElement.textContent = currentQuestion.question
+    for (i = 0; i < currentQuestion.choices.length; i++) {
+        var currentanswer = currentQuestion.choices[i]
+        var questionbtn = document.createElement("button")
+        questionbtn.textContent = currentanswer
+        answers.append(questionbtn)
+        questionbtn.addEventListener("click", checkAnswer)
 
     }
 
 }
 
 function checkAnswer(event) {
-console.log(event.target)
+    console.log(event.target)
+    var selectedAnswer = event.target.textContent;
+    var currentQuestion = questions[QuestionIndex];
+
+    if (selectedAnswer === currentQuestion.answer) {
+        score++;
+    }
+
+    QuestionIndex++;
+
+    if (QuestionIndex < questions.length) {
+        setNextQuestion();
+    } else {
+        endQuiz();
+    }
 }
+    
+
 function startTimer() {
 
 }
@@ -76,6 +102,13 @@ function startQuiz() {
     setNextQuestion()
 }
 
+function endQuiz() {
+    // You can implement the logic for ending the quiz here
+    // For example, displaying the score or saving it
+    resultContainer.textContent = "Your Score: " + score;
+    questionsContainer.classList.add("hidden");
+    resultContainer.classList.remove("hidden");
+}
 
 
 
